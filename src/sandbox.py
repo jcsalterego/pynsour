fc13327657f85c1d3444ece5bc98cb3fab62db7c
Jerry Chen <github@jcsalterego.otherinbox.com>
2009-01-13 09:43:12 -0600

    Massive overhaul on script handling in Sandbox

--- src/sandbox.py
+++ src/sandbox.py
@@ -2,8 +2,10 @@
 
 import __builtin__
 import botcode
+import md5
 import new
 import os
+import traceback
 
 SCRIPT_ROOT = "./scripts/"
 
@@ -15,7 +17,7 @@ class Sandbox:
     def __init__(self):
         """Constructor
         """
-        pass
+        self.scripts = {}
 
     def execute(self, script_ops):
         ops = []
@@ -25,53 +27,118 @@ class Sandbox:
             sender, action, recipient, msg = args
 
             script_path = "%s%s" % (SCRIPT_ROOT, script_path)
-
             if not os.path.exists(script_path):
                 continue
 
-            script = file(script_path).read()
-            script = script.replace("\r", "") + "\n\n"
-        
+            read_from_disk = True
+
+            if (script_path in self.scripts
+                and (self.scripts[script_path]['st_mtime'] ==
+                     os.stat(script_path).st_mtime)):
+
+                # don't reread from disk if it has a newer modified time
+                read_from_disk = False
+
+            if read_from_disk:
+                script = file(script_path).read()
+                script = script.replace("\r", "") + "\n\n"
+                st_mtime = os.stat(script_path).st_mtime
+
+                try:
+                    compiled = compile(script, '<string>', 'exec')
+                except:
+                    # script error
+                    ops += (botcode.OP_ERROR, (traceback.format_exc())),
+                    continue
+
+                # set up a main module
+                main = new.module('__main__')
+                main.__builtins__ = __builtin__
+
+                self.scripts[script_path] = { 'compiled': compiled,
+                                              'main': main,
+                                              'script': script,
+                                              'st_mtime': st_mtime }
+            else:
+                compiled = self.scripts[script_path]['compiled']
+                main = self.scripts[script_path]['main']
+
+            # add or update environment parameters
+            main.__dict__['sender'] = Sandbox.User(sender)
+            main.__dict__['recipient'] = recipient
+            if recipient[0] == '#':
+                main.__dict__['channel'] = recipient
+            else:
+                main.__dict__['channel'] = None
+
+            main.__dict__['action'] = action
+            main.__dict__['msg'] = msg
+            main.__dict__['argv'] = [arg for arg in msg.split(" ") if arg]
+                
+            # shorthand parameters
+            main.__dict__['recp'] = recipient
+                
+            # irc module
+            main.__dict__['irc'] = Sandbox.Irc()
+
+            # execute the code
             try:
-                compiled = compile(script, '<string>', 'exec')
+                exec compiled in main.__dict__
+                ops += main.__dict__['irc'].ops
             except:
                 # script error
                 ops += (botcode.OP_ERROR, (traceback.format_exc())),
-                continue
-
-            # set up a main module
-            main = new.module('__main__')
-            import __builtin__
-            main.__builtins__ = __builtin__
-
-            # add the parameters
-            main.__dict__['sender'] = sender
-            main.__dict__['recipient'] = recipient
-            main.__dict__['action'] = action
-
-            # shorthand parameters
-            main.__dict__['from'] = sender
-            main.__dict__['recp'] = recipient
-            main.__dict__['cmd'] = action
-
-            # irc module
-            main.__dict__['irc'] = self.irc()
-
-            # execute the code
-            exec compiled in main.__dict__
-
-            ops += main.__dict__['irc'].ops
 
         return ops
 
-    class irc:
+    class User:
+        """Interface for User
+        """
+        def __init__(self, whois_info):
+            """Constructor
+            """
+            self.nick = ""
+            self.ident = ""
+            self.has_ident = False
+            self.hostname = ""
+
+            # this could be done via regex but this will suffice for now
+            words = whois_info.split("!", 1)
+            if len(words) != 2:
+                return
+
+            self.nick, remainder = words
+
+            words = remainder.split("@", 1)
+            if len(words) != 2:
+                return
+
+            self.ident, self.hostname = words
+
+            # not sure if the tilde is part of the RFC or just convention.
+            self.has_ident = (self.ident[0] != "~")
+            self.full = whois_info
+
+        def __repr__(self):
+            """Default representation is a nick
+            """
+            return self.nick
+
+    class Irc:
         """Interface for IRC
         """
 
         def __init__(self):
+            """Constructor
+            """
             self.ops = []
 
-        def privmsg(self, recp, msg):
+        def reset(self):
+            """Reset botcode ops
+            """
+            self.ops = []
+
+        def msg(self, recp, msg):
             """Private Message
 
             Sends a PRIVMSG to a channel or nick.
